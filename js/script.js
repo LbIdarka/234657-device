9cbc5a09ab65ddf7433ea855f0cb43ba8e50f729
Alena Nikolaenko <lbidarka@gmail.com>
2017-05-11 13:56:01 +0300

    добавление состояния ошибки для модального окна на js+минификация

--- js/script.js
+++ js/script.js
@@ -1,33 +1,59 @@
-var link = document.querySelector(".btn-popup");
+  var link = document.querySelector(".btn-popup");
   var popup = document.querySelector(".popup");
   var close = popup.querySelector(".close");
   var form = popup.querySelector("form");
   var introduce = popup.querySelector("[name=name-post]");
   var email = popup.querySelector("[name=email-post]");
-  var letter =popup.querySelector("[name=text-post]");
-  var storage = localStorage.getItem("email");
+  var letter = popup.querySelector("[name=text-post]");
+  var storageIntroduce = localStorage.getItem("introduce");
+  var storageEmail = localStorage.getItem("email");
 
   link.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.add("popup--show");
-    introduce.focus();
+    if (storageIntroduce && storageEmail) {
+      introduce.value = storageIntroduce;
+      email.value = storageEmail;
+      letter.focus();
+    } else {
+        introduce.focus();
+      }
   });
 
   close.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.remove("popup--show");
     popup.classList.remove("popup-error");
+    introduce.classList.remove("popup-invalid");
+    email.classList.remove("popup-invalid");
+    letter.classList.remove("popup-invalid");
   });
 
   form.addEventListener("submit", function(event) {
+    if (!introduce.value) {
+      event.preventDefault();
+      introduce.classList.add("popup-invalid");
+    } else {
+        localStorage.setItem("introduce", introduce.value);
+      }
+
+    if (!email.value) {
+      event.preventDefault();
+      email.classList.add("popup-invalid");
+    } else {
+        localStorage.setItem("email", email.value);
+      }
+
+    if (!letter.value) {
+      event.preventDefault();
+      letter.classList.add("popup-invalid");
+    }
+
     if (!introduce.value || !email.value || !letter.value) {
       event.preventDefault();
-      console.log("Заполните все поля, пожалуйста");
       popup.classList.remove("popup-error");
       popup.offsetWidth = popup.offsetWidth;
       popup.classList.add("popup-error");
-    } else {
-      localStorage.setItem("email", email.value);
     }
   });
 
@@ -36,6 +62,9 @@ var link = document.querySelector(".btn-popup");
       if (popup.classList.contains("popup--show")) {
         popup.classList.remove("popup--show");
         popup.classList.remove("popup-error");
+        introduce.classList.remove("popup-invalid");
+        email.classList.remove("popup-invalid");
+        letter.classList.remove("popup-invalid");
       }
     }
   });
@@ -65,24 +94,23 @@ var link = document.querySelector(".btn-popup");
   });
 
 var myMap;
-var myPlacemark;
 
 // Дождёмся загрузки API и готовности DOM.
 ymaps.ready(init);
 
 function init () {
-    // Создание экземпляра карты и его привязка к контейнеру с
-    // заданным id ("map").
-    myMap = new ymaps.Map('yandex-map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center: [55.68697956906804,37.52965449999998], // Москва
-        zoom: 17
-    }, {
-        searchControlProvider: 'yandex#search'
-    });
+  // Создание экземпляра карты и его привязка к контейнеру с
+  // заданным id ("map").
+  myMap = new ymaps.Map("yandex-map", {
+    // При инициализации карты обязательно нужно указать
+    // её центр и коэффициент масштабирования.
+    center: [55.68697956906804,37.52965449999998], // Москва
+    zoom: 17
+  }, {
+      searchControlProvider: "yandex#search"
+  });
 
-     myMap.geoObjects
-        .add(new ymaps.Placemark([55.68712503147963,37.52969741534421]))
+  myMap.geoObjects
+    .add(new ymaps.Placemark([55.68712503147963,37.52969741534421]))
 }
 
